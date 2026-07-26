5a64dd94bfc43045616456a165461aa6a6560a6a
Kelz <msayala.2015@gmail.com>
2026-07-25 20:19:57 -0400

    Configure Astro project for Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config"
 import mdx from "@astrojs/mdx"
-import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
 
@@ -9,7 +8,6 @@ export default defineConfig({
 
   integrations: [
     mdx(),
-    //sitemap(),
     solidJs(),
     tailwind({ applyBaseStyles: false })
   ],
